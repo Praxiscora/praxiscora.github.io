5e810e92f69a82f998dfb13a68b894003c5e3422
Praxiscora <ludwig.logopaedie@gmail.com>
2015-03-30 21:45:11 +0200

    jQuery

---
+++ therapie.js
@@ -0,0 +1,15 @@
+
+var meisi = ( function() {
+   
+   $(document).ready(function() {
+    $("#cf7_controls").on('click', 'span', function() {
+     $("#cf7 img").removeClass("opaque");
+     var newImage = $(this).index();
+
+     $("#cf7 img").eq(newImage).addClass("opaque");
+
+     $("#cf7_controls span").removeClass("selected");
+     $(this).addClass("selected");
+    });
+   });
+})(jQuery)
